9ae74426d0bc512c933ca4e800b64403192e199e
Harrison Shoebridge <harrison@theshoebridges.com>
2018-07-03 00:41:59 +1000

    Add in (pretty broken) cube fly around code

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,5 +1,4 @@
-uint64 faceLength = 30;
-
+// Voxel direction/faces
 enum {
   BACK,
   FRONT,
@@ -9,6 +8,36 @@ enum {
   TOP,
   MAX_FACE
 };
+
+// Rotation states
+enum {
+  ROT_IDLE,
+  ROT_FORWARD,
+  ROT_BACKWARD,
+  ROT_LEFT,
+  ROT_RIGHT
+};
+
+#define ROTATION_DURATION 500
+
+struct GameState {
+  uint8 world[WORLD_HEIGHT][WORLD_DEPTH][WORLD_WIDTH];
+  uint64 vertCount;
+
+  int64 progress;
+
+  GLuint VAO, VBO, EBO;
+
+  quat cameraRotation;
+
+  vec3 rotFBUp;
+  vec3 rotLRUp;
+  quat rotStart;
+  uint32 rotState;
+  uint32 rotStartTime;
+};
+
+uint64 faceLength = 30;
 
 bool voxelEmptyToThe(int d, uint8 w[WORLD_HEIGHT][WORLD_DEPTH][WORLD_WIDTH], int x, int y, int z) {
   int offsetX = 0;
@@ -80,18 +109,17 @@ void addFaceToMesh(uint32 d, real32* verts, uint64* len, vec3 offset) {
   }
 }
 
-struct GameState {
-  uint8 world[WORLD_HEIGHT][WORLD_DEPTH][WORLD_WIDTH];
-  uint64 vertCount;
-
-  int64 progress;
-
-  GLuint VAO, VBO, EBO;
-};
-
 void gameStateInit(State* state) {
   LornockMemory* m = lornockMemory;
   GameState* g = (GameState*) state->memory;
+
+  g->cameraRotation = quatFromAxisAngle(vec3(1, 0, 0), 0);
+
+  g->rotFBUp = vec3(1, 0, 0);
+  g->rotLRUp = vec3(0, 1, 0);
+
+  g->rotState = ROT_IDLE;
+  g->rotStartTime = timeNow();
 
   // init world
   for (int y = 0; y < WORLD_HEIGHT; y++) {
@@ -156,6 +184,101 @@ void gameStateInit(State* state) {
 void gameStateUpdate(State* state) {
   GameState* g = (GameState*) state->memory;
 
+  /* updating */
+
+  int32 rotDir = 1;
+  real32 rotation = 0;
+
+  vec3 up = vec3(0.0f, 0.0f, 0.0f);
+
+  switch (g->rotState) {
+    case ROT_IDLE:
+      {
+        bool starting = false;
+        if (keyJustDown(KEY_w)) {
+          g->rotState = ROT_FORWARD;
+
+          starting = true;
+        } else if (keyJustDown(KEY_s)) {
+          g->rotState = ROT_BACKWARD;
+
+          starting = true;
+        } else if (keyJustDown(KEY_a)) {
+          g->rotState = ROT_LEFT;
+          
+          starting = true;
+        } else if (keyJustDown(KEY_d)) {
+          g->rotState = ROT_LEFT;
+
+          starting = true;
+        }
+
+        if (starting) {
+          g->rotStart = g->cameraRotation;
+          g->rotStartTime = timeNow();       
+        }
+
+        break;
+      }
+    case ROT_FORWARD:
+      {
+        up = g->rotFBUp;
+        rotDir = 1;
+      } break;
+    case ROT_BACKWARD:
+      {
+        up = g->rotFBUp;
+        rotDir = -1;
+      } break;
+    case ROT_LEFT:
+      {
+        up = g->rotLRUp;
+        rotDir = 1;
+      } break;
+    case ROT_RIGHT:
+      {
+        up = g->rotLRUp;
+        rotDir = -1;
+      } break;
+  }
+
+  if (!g->rotState == ROT_IDLE) {
+    if (g->rotStartTime + ROTATION_DURATION < timeNow()) {
+      g->rotState = ROT_IDLE;
+
+      /*
+
+      vec4 fb4 = vec4(g->rotFBUp.x, g->rotFBUp.y, g->rotFBUp.z, 0);
+      vec4 lr4 = vec4(g->rotLRUp.x, g->rotLRUp.y, g->rotLRUp.z, 0);
+
+      vec4 fb = vec4Normalize(mat4Rotate(mat4d(1.0f), deg2Rad(90 * rotDir), up) * fb4);
+      g->rotFBUp = vec3(fb.x, fb.y, fb.z);
+
+      vec4 lr = vec4Normalize(mat4Rotate(mat4d(1.0f), deg2Rad(90 * rotDir), up) * lr4);
+      g->rotLRUp = vec3(lr.x, lr.y, lr.z);
+
+      logfln("%f %f %f", g->rotLRUp.x, g->rotLRUp.y, g->rotLRUp.z);*/
+    } else {
+      real32 start = 0;
+      real32 end = 90;
+
+      real32 pc = 1.0f - ((float) ((g->rotStartTime + ROTATION_DURATION) - timeNow())) / ROTATION_DURATION;
+      logfln("%f", pc);
+
+      rotation = pc * end;
+
+      quat rot = quatFromAxisAngle(up, deg2Rad(rotation * rotDir));
+
+      g->cameraRotation = g->rotStart * rot;
+    }
+  }
+
+  /*
+  quat diff = quatFromAxisAngle(vec3(0, 1, 0), deg2Rad(10.f));
+  g->cameraRotation = quatFromAxisAngle(vec3(1, 0, 0), deg2Rad(rotation)) * diff;*/
+
+  /* drawing */
+
   glClearColor(0.0f, 0.58f, 0.93f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -171,11 +294,13 @@ void gameStateUpdate(State* state) {
 
   vec3 dir = vec3Normalize(cameraPosition - asteroidPosition);
 
+  mat4 rotMat4 = quatToMat4(g->cameraRotation);
+
   mat4 view = mat4d(1);
   real32 deg;
-  // replace with: https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
+  // TODO(harrison): replace with: https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
   view = mat4Translate(view, cameraPosition);
-  view = mat4Rotate(view, deg2Rad(timeNow()*10), vec3(1, 0, 0));
+  view = view * rotMat4;
 
   mat4 projection = mat4Perspective(70.0f, 640.0f/480.0f, 0.1f, 10000.0f);
 
--- src/hmm_wrapper.cpp
+++ src/hmm_wrapper.cpp
@@ -2,11 +2,16 @@ typedef hmm_v2 vec2;
 typedef hmm_v3 vec3;
 typedef hmm_v4 vec4;
 typedef hmm_m4 mat4;
+typedef hmm_quaternion quat;
 
 #define vec2(x, y)        HMM_Vec2(x, y)
 #define vec3(x, y, z)     HMM_Vec3(x, y, z)
 #define vec4(x, y, z, w)  HMM_Vec4(x, y, z, w)
 #define mat4d(d) HMM_Mat4d(d)
+
+#define quatToMat4(q) HMM_QuaternionToMat4(q)
+
+#define quatFromAxisAngle(axis, angle) HMM_QuaternionFromAxisAngle(axis, angle)
 
 #define mat4Perspective(fov, ratio, near, far) HMM_Perspective(fov, ratio, near, far)
 #define mat4Orthographic(left, right, bottom, top, near, far) HMM_Orthographic(left, right, bottom, top, near, far)
@@ -17,3 +22,4 @@ typedef hmm_m4 mat4;
 #define mat4Rotate(m, a, v) HMM_Multiply(m, HMM_Rotate(a, v))
 
 #define vec3Normalize(v) HMM_NormalizeVec3(v)
+#define vec4Normalize(v) HMM_NormalizeVec4(v)
